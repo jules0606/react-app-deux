4149145f4708bf2d6e4c8b6b07dd86135aba117f
jules0606 <jules.baumann0606@gmail.com>
2025-05-15 15:52:10 +0200

    completed 16 chapters of tutorial + remove no-unused-vars for build

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+import styles from '@/app/ui/home.module.css'; // eslint-disable-line no-unused-vars
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
@@ -28,7 +28,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
